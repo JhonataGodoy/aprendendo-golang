3708045d24d63fa3f7632f2bd2ff6256b64afbce
JhonataGodoy <jhonata.godoy@mercadolivre.com>
2022-11-16 17:03:34 -0300

    Subindo arquivos alterado.

--- inteiros/adicionador_test.go
+++ inteiros/adicionador_test.go
@@ -4,11 +4,29 @@ import (
 	"testing"
 )
 
-func TestAdicionador(t *testing.T) {
-	soma := Adiciona(2, 2)
-	esperado := 4
+func TestSomarFloat(t *testing.T) {
+	var soma float64 = Adiciona(1.5, 2.7)
+	esperado := 4.2
 
 	if soma != esperado {
-		t.Errorf("esperado '%d', resultado '%d'", esperado, soma)
+		t.Errorf("esperado '%f', resultado '%f'", esperado, soma)
+	}
+}
+
+func TestSomarPositivoENegativo(t *testing.T) {
+	soma := Adiciona(-2, 2)
+	esperado := 0.0
+
+	if soma != esperado {
+		t.Errorf("esperado '%f', resultado '%f'", esperado, soma)
+	}
+}
+
+func TestAdicionador(t *testing.T) {
+	soma := Adiciona(4, 5)
+	esperado := 9.0
+
+	if soma != esperado {
+		t.Errorf("esperado '%f', resultado '%f'", esperado, soma)
 	}
 }
