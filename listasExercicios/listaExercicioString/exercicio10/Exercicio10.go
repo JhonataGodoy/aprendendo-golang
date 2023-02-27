e615cedf54eda9b36d189e709b72acce3fd5a7e6
JhonataGodoy <jhonata.godoy@mercadolivre.com>
2023-02-27 18:13:42 -0300

    up-to-date exercises strings

--- listasExercicios/listaExercicioString/exercicio10/Exercicio10.go
+++ listasExercicios/listaExercicioString/exercicio10/Exercicio10.go
@@ -6,15 +6,109 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("enter your Cpf:")
-	pickUpCpf, _ := reader.ReadString('\n')
-	wipeCpf := strings.TrimSpace(pickUpCpf)
-	fmt.Println(wipeCpf)
+	fmt.Println("number up to 99:")
+	pickUpNumber, _ := reader.ReadString('\n')
+	wipeNumber := strings.TrimSpace(pickUpNumber)
+
+	if wipeNumber == "1" {
+		fmt.Println("um")
+	} else if wipeNumber == "2" {
+		fmt.Println("dois")
+	} else if wipeNumber == "3" {
+		fmt.Println("tres")
+	} else if wipeNumber == "4" {
+		fmt.Println("quatro")
+	} else if wipeNumber == "5" {
+		fmt.Println("cinco")
+	} else if wipeNumber == "6" {
+		fmt.Println("seis")
+	} else if wipeNumber == "7" {
+		fmt.Println("sete")
+	} else if wipeNumber == "8" {
+		fmt.Println("oito")
+	} else if wipeNumber == "9" {
+		fmt.Println("nove")
+	}
+
+	if string(wipeNumber[0]) == "1" {
+		if wipeNumber == "10" {
+			fmt.Println("dez")
+		} else if wipeNumber == "11" {
+			fmt.Println("onze")
+		} else if wipeNumber == "12" {
+			fmt.Println("doze")
+		} else if wipeNumber == "13" {
+			fmt.Println("treze")
+		} else if wipeNumber == "14" {
+			fmt.Println("quatorze")
+		} else if wipeNumber == "15" {
+			fmt.Println("quinze")
+		} else if wipeNumber == "16" {
+			fmt.Println("dezesseis")
+		} else if wipeNumber == "17" {
+			fmt.Println("dezessete")
+		} else if wipeNumber == "18" {
+			fmt.Println("dezoito")
+		} else if wipeNumber == "19" {
+			fmt.Println("dezenove")
+		}
+	}
+
+	var ExtensiveStart string
+	if len(wipeNumber) == 2 {
+		getsDecimal := string(wipeNumber[0])
+		getsUnit := string(wipeNumber[1])
+		convertDecimal, _ := strconv.Atoi(getsDecimal)
+		convertUnit, _ := strconv.Atoi(getsUnit)
+		if convertDecimal == 2 && string(wipeNumber[0]) != "1" {
+			ExtensiveStart = "vinte"
+		} else if convertDecimal == 3 {
+			ExtensiveStart = "trinta"
+		} else if convertDecimal == 4 {
+			ExtensiveStart = "quarenta"
+		} else if convertDecimal == 5 {
+			ExtensiveStart = "cinquenta"
+		} else if convertDecimal == 6 {
+			ExtensiveStart = "sessenta"
+		} else if convertDecimal == 7 {
+			ExtensiveStart = "setenta"
+		} else if convertDecimal == 8 {
+			ExtensiveStart = "oitenta"
+		} else if convertDecimal == 9 {
+			ExtensiveStart = "noventa"
+		}
+
+		if string(wipeNumber[0]) != "1" {
+			if convertUnit == 1 {
+				ExtensiveStart += " e um"
+			} else if convertUnit == 2 {
+				ExtensiveStart += " e dois"
+			} else if convertUnit == 3 {
+				ExtensiveStart += " e tres"
+			} else if convertUnit == 4 {
+				ExtensiveStart += " e quatro"
+			} else if convertUnit == 5 {
+				ExtensiveStart += " e cinco"
+			} else if convertUnit == 6 {
+				ExtensiveStart += " e seis"
+			} else if convertUnit == 7 {
+				ExtensiveStart += " e sete"
+			} else if convertUnit == 8 {
+				ExtensiveStart += " e oito"
+			} else if convertUnit == 9 {
+				ExtensiveStart += " e nove"
+			}
+		}
+
+	}
+
+	fmt.Println(ExtensiveStart)
 
 }
--- listasExercicios/listaExercicioString/exercicio11/Exercicio11.go
+++ listasExercicios/listaExercicioString/exercicio11/Exercicio11.go
@@ -4,18 +4,38 @@ package main
 //e escolherá uma aleatoriamente. O jogador poderá errar 6 vezes antes de ser enforcado.
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
-	"log"
+	"io"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
 )
 
 func main() {
 	abreviaCaminho := "/Users/jhgodoy/Documents/Transport Core/aprendendo-golang/listasExercicios/listaExercicioString/exercicio11/Exercicio11.txt"
-	file, err := ioutil.ReadFile(abreviaCaminho)
+	openFile, err := os.Open(abreviaCaminho)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	text := string(file)
-	fmt.Println(text)
+	rand.Seed(time.Now().UnixNano())    // inicializa numeros aleatorios
+	defer openFile.Close()              // fecha o arquivo lido na ultima linha da func
+	reader := bufio.NewReader(openFile) // le o arquivo
+	var pickUpWords []string
+	for true {
+		pickUpString, err := reader.ReadString('\n') // Aqui verifica se o arquivo existe, encontra a string vazia ' '
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+		pickUpString = strings.Trim(pickUpString, "\n") // tira a string que eu quero, no caso o \n
+		pickUpWords = append(pickUpWords, pickUpString) // soma
+	}
+	numberElements := len(pickUpWords)
+	randomNumber := rand.Intn(numberElements) // numeros aleatorios
 
+	fmt.Println(pickUpWords[randomNumber])
 }
